fd09ab45e849497901e21d26faecbcc9c374f7b1
Dejw125 <dstawowy@gmail.com>
2018-04-16 11:07:33 +0100

    Order and Packing Station classes

--- 0.1.Simulator/src/model/Order.java
+++ 0.1.Simulator/src/model/Order.java
@@ -1,5 +1,79 @@
 package model;
 
+import java.util.HashSet;
+
+/**
+ * Represents a single order for an automated warehouse
+ *
+ * @author Nathan32
+ * 
+ * TODO: does anything need to be added? it seems fully functional to me
+ *
+ */
 public class Order {
 
+	private int packingTime;
+	private HashSet<String> shelfUIDs;
+	private String status;
+
+	public Order(int pt, HashSet<String> SUIDs) {
+		packingTime = pt;
+		shelfUIDs = SUIDs;
+		status = "unassigned";
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * Tells the order that it has now been assigned
+	 *
+	 */
+	public void assignOrder() {
+		status = "assigned";
+	}
+
+	/**
+	 * gets a HashSet with the required items for the order
+	 *
+	 * @return IDs of shelves that are needed
+	 */
+	public HashSet<String> requiredItems(){
+		return shelfUIDs;
+	}
+
+	/**
+	 * Gets the order to progress one step, and see if it has been packed
+	 *
+	 * @return true if item has finished, false if it still needs packing time
+	 */
+	public boolean progress() {
+		packingTime--;
+		if (packingTime == 0) {
+			status = "dispatched";
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Removes an item from the set of required items
+	 *
+	 * @param collectedItem shelfID that the item has been "collected" from
+	 */
+	public void itemGained(String collectedItem) {
+		shelfUIDs.remove(collectedItem);
+	}
+
+	public String toString(){
+		String str = "Shelf: ";
+		for (String string : shelfUIDs) {
+			str+= string + ", ";
+		}
+		str += "Status: " + status;
+		str += "Packing Time: " + packingTime;
+
+		return str;
+	}
 }
--- 0.1.Simulator/src/model/PackingStation.java
+++ 0.1.Simulator/src/model/PackingStation.java
@@ -1,5 +1,85 @@
+//seems good, no need to make edits? Dawid
+
 package model;
 
-public class PackingStation {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class PackingStation extends Entity {
+
+	private Order currentOrder;
+	private HashSet<String> requestedItems;
+	private ArrayList<Robot> robots;
+
+
+	public PackingStation(String ID, Location loc) {
+		super(ID, loc);
+		robots = new ArrayList<Robot>();
+		requestedItems = new HashSet<String>();
+	}
+
+	@Override
+	public void act(Grid grid, List<Order> orders) {
+		// check if it has an order
+		if (currentOrder != null) {
+			//if so, see what needs to happen to it
+			if (currentOrder.requiredItems().isEmpty()) {
+				//the order has all of the items needed, pack/dispatch
+				if (currentOrder.progress()) {
+					//if order is complete, remove order
+					currentOrder = null;
+				}
+			} else {//needs items brought to it
+
+				// use unrequested items to ask robots for those items
+				HashSet<String> items = new HashSet<String>();
+				items.addAll(currentOrder.requiredItems());
+				items.removeAll(requestedItems);
+				requestItems(items, grid);
+
+				//Check if there is a robot in its location that has the item
+				Robot r = grid.getRobot(location);
+				if (r != null ) {
+					if (r.hasItem(requestedItems)){
+						String collectedItem = r.carriedItem();
+						currentOrder.itemGained(collectedItem);
+						requestedItems.remove(collectedItem);
+					}
+				}
+
+			}
+		} else {//needs a new order
+			currentOrder = orders.get(0);
+			orders.remove(0);
+			//ask sim for a new order
+		}
+	}
+
+	/**
+	 * Informs the packing station of a robot within the simulation
+	 *
+	 * @param robot A robot that exists within the simulation
+	 */
+	public void addRobot(Robot robot) {
+		robots.add(robot);
+	}
+
+	private void requestItems(HashSet<String> items, Grid grid) {
+		//Change to do per item rather than per robot
+		for (Robot r : robots) {
+			if (r.getStatus().equals("ready")) {
+				for (String item : items) {
+					if (r.takeJob(item, grid, location)) {
+						requestedItems.add(item);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+
+
 
 }
